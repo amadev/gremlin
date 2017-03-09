83510a792fc7a8eda6f62679269f27474dd3792b
Andrey Volkov <avolkov@mirantis.com>
2017-03-09 14:47:53 +0300

    Add clojure project template

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject migration_tests "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot migration-tests.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
