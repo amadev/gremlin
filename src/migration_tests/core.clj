48ba0d0961f73fb0562c887f160d7529f9aee3bb
Andrey Volkov <avolkov@mirantis.com>
2017-03-09 16:36:56 +0300

    Execute sql file

--- src/migration_tests/core.clj
+++ src/migration_tests/core.clj
@@ -1,5 +1,6 @@
 (ns migration-tests.core
-  (:require [clojure.java.jdbc :as jdbc])
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as str])
   (:gen-class))
 
 (def db-spec
@@ -9,11 +10,20 @@
    :user "root"
    :password "56592b2f97c7de918edd"})
 
+(defn skip-line? [line]
+  (let [line (str/trim line)]
+   (or (empty? line) (str/starts-with? line "--"))))
+
+(defn split-sql-statements [text]
+  "Naive way of splitting sql statements.
+   Strings with semicolons and multi-line comments are not supported"
+  (remove skip-line? (str/split text #";" )))
+
 (defn exec-sql-file [file-name]
-  (jdbc/query db-spec ["SELECT * FROM instances"]))
+  (jdbc/db-do-commands db-spec (split-sql-statements (slurp file-name))))
 
 (defn -main
   [& args]
   (let [case "add_index"]
-    (exec-sql-file (str "cases/" case "init.sql"))
-    (println "Hello, World!")))
+    (exec-sql-file (str "cases/" case "/init.sql"))
+    (println "Done!")))
